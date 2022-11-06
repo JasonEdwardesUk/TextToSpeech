e2acd9bf0d2a535bfcc722ea2d9bd87af9cd1055
JasonEdwardesUk <jasonedwardes@gmail.com>
2022-11-06 20:37:40 +0000

    Text to speech works starting to add speech to text

--- app/src/main/java/com/jasonedwardes/texttospeech/MainActivity.java
+++ app/src/main/java/com/jasonedwardes/texttospeech/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 
 import com.jasonedwardes.texttospeech.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity  {
 
     ActivityMainBinding binding;
+    TextToSpeech textToSpeech;
 
+   // Locale locale = new Locale("ENGLAND");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +25,28 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         setContentView(view);
         // check view.binding is working
         //binding.idTxt.setText("test");
-    }
 
-    @Override
-    public void onClick(View view) {
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS) {
+                    //could add array of Languages on spinner.
 
+                   // textToSpeech.setLanguage(locale);
+                   textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+
+            }
+
+            //binding.idBtn.setOnClickListener();
+        });
+            binding.idBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s = binding.idEditTxt.getText().toString();
+                     textToSpeech.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+
+                }
+            });
     }
 }
