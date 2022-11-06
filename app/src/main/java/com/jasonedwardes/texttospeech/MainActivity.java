f6360fce83873eeb89bc3f67b1708f1a5a17ff03
JasonEdwardesUk <jasonedwardes@gmail.com>
2022-11-06 22:36:12 +0000

    speech to text landscape works but a little buggy

--- app/src/main/java/com/jasonedwardes/texttospeech/MainActivity.java
+++ app/src/main/java/com/jasonedwardes/texttospeech/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jasonedwardes.texttospeech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 
@@ -26,27 +29,52 @@ public class MainActivity extends AppCompatActivity  {
         // check view.binding is working
         //binding.idTxt.setText("test");
 
+//text to speech////////////////////////////////////////////
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int i) {
                 if (i == TextToSpeech.SUCCESS) {
                     //could add array of Languages on spinner.
 
-                   // textToSpeech.setLanguage(locale);
-                   textToSpeech.setLanguage(Locale.ENGLISH);
+                    // textToSpeech.setLanguage(locale);
+                    textToSpeech.setLanguage(Locale.ENGLISH);
                 }
 
             }
 
             //binding.idBtn.setOnClickListener();
         });
-            binding.idBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String s = binding.idEditTxt.getText().toString();
-                     textToSpeech.speak(s, TextToSpeech.QUEUE_FLUSH, null);
 
-                }
-            });
+        binding.idBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = binding.idEditTxt.getText().toString();
+                textToSpeech.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+
+            }
+        });
+        ///////////////////////////end of text to speech//////////////////////
+        ////////////////////start of speech to text///////////////////////////////
+        binding.idBtnSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start speaking");
+                startActivityForResult(intent, 102);
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @NonNull Intent data){
+        super.onActivityResult(requestCode, resultCode,data);
+
+        if (requestCode == 102 && resultCode == RESULT_OK){
+            binding.idTxt.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
+
+        }
+    }
+
 }
